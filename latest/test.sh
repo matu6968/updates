81cffe39c3d043cfd457f4685fe8f20cca6d1b41
matik <89382946+matu6968@users.noreply.github.com>
2023-11-09 00:21:09 +0100

    temporary test update script file

---
+++ latest/test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+echo "fucked up head & BPD"
+sleep 1.5
+echo "girl ik u wanted me"
+sleep 1.5
+echo "wanna see me in the dirt"
+sleep 1.5
+echo "wanna see me in a skirt"
+sleep 1.5 
+echo "i wanna be cute like u"
+sleep 1.5
+echo "let me feel ur molecules"
+sleep 1.5
+echo "f me hard inside the church"
+sleep 1.5
+echo "then ill show u my cute skirt"
+sleep 1.5
+echo "girl u make me so warm"
+sleep 1.5
+echo "without u im so bored"
+sleep 1.5
+echo "ur voice is stuck inside of my head"
+sleep 1.5
+echo "my blood spilled its rly red"
+sleep 1.5
+echo "come right here n lick it up"
+sleep 1.5
+echo "kiss my neck n f me up"
+sleep 1.5
+echo "f me till the suns up"
+sleep 1.5
+echo "f me till i throw up"
+sleep 1.5
+echo "ur so upset w me"
+sleep 1.5
+echo "but im so obsessed w u"
+sleep 1.5
+echo "im fucking crazy"
+sleep 1.5
+echo "i dont wanna damage u"
+sleep 1.5
+echo "but dont fucking test me"
+sleep 1.5
+echo "my mentals unsteady"
+sleep 1.5
+echo "gonna kms yeah" 
+sleep 1.5
+echo "just like rebzyy"
+sleep 1.5
